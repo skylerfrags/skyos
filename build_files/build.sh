4294efc97d84b61a9ec671eb9af5540833048156
skylerfrags <quadfps@gmail.com>
2025-08-29 02:24:06 +0100

    added hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,20 +4,22 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Fedora Repo
+dnf5 install -y tmux \
+	pavucontrol \
+	kitty \
+	sddm \
+	xorg-x11-server-Xwayland \
+	waybar
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# Hyprland
+dnf5 copr enable solopasha/hyprland
+dnf5 install -y hyprland \ 
+	xdg-desktop-portal-hyprland \
+	hyprpolkitagent \
+	hyprshot \
+	swww \
+	waypaper
 
 #### Example for enabling a System Unit File
 
